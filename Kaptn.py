8c01dbda62e8208c6628cdf6474d08422bd7106d
Merthan1 <91878146+Merthan1@users.noreply.github.com>
2021-10-05 09:00:44 +0200

    Add files via upload

---
+++ Kaptn.py
@@ -0,0 +1,64 @@
+import random
+import time
+
+range = pirat=random.randint(1,100)
+
+print("arrrrrgghh ich bin Käptn Hook und denke an eine Zahl, wenn du sie errätst, kriegst du einen Schatz! du hast nur 6 Versuche.\n")
+time.sleep(0.5)
+Zahl=int(input("Wähle eine Zahl zwischen 1-100\n"))
+
+if pirat==Zahl:
+    print("DU HAST DIE BEUTE ERLANGT!!! ARRRGGGGGHHH")
+    exit()
+
+if Zahl < pirat:
+    print("Zu niedrig du Holzbein!")
+else: print("zu hoch mein Sohnemann.")
+
+Zahl=int(input("Du hast noch 5 Versuche Kamerad!\n"))
+if pirat==Zahl:
+    print("DU HAST DIE BEUTE ERLANGT!!! ARRRGGGGGHHH")
+    exit()
+
+if Zahl < pirat:
+    print("Zu niedrig du Holzbein!")
+else: print("zu hoch mein Sohnemann.")
+
+Zahl=int(input("Du hast noch 4 Versuche Kamerad!\n"))
+if pirat==Zahl:
+    print("DU HAST DIE BEUTE ERLANGT!!! ARRRGGGGGHHH")
+    exit()
+
+if Zahl < pirat:
+    print("Zu niedrig du Holzbein!")
+else: print("zu hoch mein Sohnemann.")
+
+Zahl=int(input("Du hast noch 3 Versuche Kamerad!\n"))
+if pirat==Zahl:
+    print("DU HAST DIE BEUTE ERLANGT!!! ARRRGGGGGHHH")
+    exit()
+
+if Zahl < pirat:
+    print("Zu niedrig du Holzbein!\n")
+else: print("zu hoch mein Sohnemann.\n")
+Zahl=int(input("Du hast noch 2 Versuche Kamerad!\n"))
+if pirat==Zahl:
+    print("DU HAST DIE BEUTE ERLANGT!!! ARRRGGGGGHHH")
+    exit()
+
+if Zahl < pirat:
+    print("Zu niedrig du Holzbein!\n")
+else: print("zu hoch mein Sohnemann.\n")
+Zahl=int(input("Du hast noch 1 Versuche Kamerad!\n"))
+if pirat==Zahl:
+    print("DU HAST DIE BEUTE ERLANGT!!! ARRRGGGGGHHH")
+    exit()
+
+if Zahl < pirat:
+    print("Zu niedrig du Holzbein!\n")
+else: print("zu hoch mein Sohnemann.\n")
+print("Du hast verloren du Jammerlappen, Versuch es nochmal!")
+exit()
+        
+         
+         
